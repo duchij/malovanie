b717ddaacbbeccb9765a558f4f9cb4d81ab6cb14
Boris Duchaj <bduchaj@gmail.com>
2018-03-16 14:10:10 +0100

    Add project files.

---
+++ WindowsFormsApp11/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace WindowsFormsApp11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btn_pen = new System.Windows.Forms.Button();
+            this.btn_square = new System.Windows.Forms.Button();
+            this.btn_circle = new System.Windows.Forms.Button();
+            this.pColor = new System.Windows.Forms.Panel();
+            this.bColorPick = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.pCanvas = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pCanvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.btn_pen);
+            this.panel1.Controls.Add(this.btn_square);
+            this.panel1.Controls.Add(this.btn_circle);
+            this.panel1.Controls.Add(this.pColor);
+            this.panel1.Controls.Add(this.bColorPick);
+            this.panel1.Location = new System.Drawing.Point(8, 8);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(133, 461);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(10, 192);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Ulozit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_pen
+            // 
+            this.btn_pen.Location = new System.Drawing.Point(10, 127);
+            this.btn_pen.Name = "btn_pen";
+            this.btn_pen.Size = new System.Drawing.Size(62, 23);
+            this.btn_pen.TabIndex = 3;
+            this.btn_pen.Text = "Pero";
+            this.btn_pen.UseVisualStyleBackColor = true;
+            this.btn_pen.Click += new System.EventHandler(this.selectObject_Click);
+            // 
+            // btn_square
+            // 
+            this.btn_square.Location = new System.Drawing.Point(59, 98);
+            this.btn_square.Name = "btn_square";
+            this.btn_square.Size = new System.Drawing.Size(62, 23);
+            this.btn_square.TabIndex = 2;
+            this.btn_square.Text = "Stvorec";
+            this.btn_square.UseVisualStyleBackColor = true;
+            this.btn_square.Click += new System.EventHandler(this.selectObject_Click);
+            // 
+            // btn_circle
+            // 
+            this.btn_circle.Location = new System.Drawing.Point(10, 98);
+            this.btn_circle.Name = "btn_circle";
+            this.btn_circle.Size = new System.Drawing.Size(43, 23);
+            this.btn_circle.TabIndex = 1;
+            this.btn_circle.Text = "Kruh";
+            this.btn_circle.UseVisualStyleBackColor = true;
+            this.btn_circle.Click += new System.EventHandler(this.selectObject_Click);
+            // 
+            // pColor
+            // 
+            this.pColor.Location = new System.Drawing.Point(48, 44);
+            this.pColor.Margin = new System.Windows.Forms.Padding(2);
+            this.pColor.Name = "pColor";
+            this.pColor.Size = new System.Drawing.Size(24, 23);
+            this.pColor.TabIndex = 0;
+            // 
+            // bColorPick
+            // 
+            this.bColorPick.Location = new System.Drawing.Point(10, 10);
+            this.bColorPick.Margin = new System.Windows.Forms.Padding(2);
+            this.bColorPick.Name = "bColorPick";
+            this.bColorPick.Size = new System.Drawing.Size(111, 31);
+            this.bColorPick.TabIndex = 0;
+            this.bColorPick.Text = "Pick color";
+            this.bColorPick.UseVisualStyleBackColor = true;
+            this.bColorPick.Click += new System.EventHandler(this.bColorPick_Click);
+            // 
+            // pCanvas
+            // 
+            this.pCanvas.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pCanvas.Location = new System.Drawing.Point(146, 8);
+            this.pCanvas.Name = "pCanvas";
+            this.pCanvas.Size = new System.Drawing.Size(644, 456);
+            this.pCanvas.TabIndex = 1;
+            this.pCanvas.TabStop = false;
+            this.pCanvas.Paint += new System.Windows.Forms.PaintEventHandler(this.pCanvas_Paint);
+            this.pCanvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pCanvas_MouseDown);
+            this.pCanvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pCanvas_MouseMove);
+            this.pCanvas.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pCanvas_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(802, 476);
+            this.Controls.Add(this.pCanvas);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResizeBegin += new System.EventHandler(this.Form1_ResizeBegin);
+            this.ResizeEnd += new System.EventHandler(this.Form1_ResizeEnd);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pCanvas)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button bColorPick;
+        private System.Windows.Forms.Panel pColor;
+        private System.Windows.Forms.Button btn_pen;
+        private System.Windows.Forms.Button btn_square;
+        private System.Windows.Forms.Button btn_circle;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.PictureBox pCanvas;
+    }
+}
+
---
+++ WindowsFormsApp11/Form1.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+
+namespace WindowsFormsApp11
+{
+
+    public struct formSize
+    {
+        public float x;
+        public float y;
+        public float width;
+        public float height;
+    }
+
+    public enum DrawType { pen=0, circle=1,square=2};
+
+    public partial class Form1 : Form
+    {
+        private bool        isMousePressed = false;
+        private Graphics    graphics = null;
+        private int         tempX = 0;
+        private int         tempY = 0;
+
+        private int startX = 0;
+        private int startY = 0;
+
+        private int _x, _y;
+
+        private Color       color = Color.Black;
+        private SolidBrush  brush = new SolidBrush(Color.Black);
+
+        private Pen         pen;
+
+        private Pen         eraser;
+
+        private Graphics    gCache = null;
+
+        private formSize    frmSize;
+
+        private DrawType    drawType = new DrawType();
+
+        private Rectangle   drawingRectangle;
+
+        private Image image;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.pCanvas.Image = new Bitmap(this.pCanvas.ClientSize.Width,this.pCanvas.ClientSize.Height);
+            this.graphics = Graphics.FromImage(this.pCanvas.Image);
+            this.gCache = this.pCanvas.CreateGraphics();
+           // this.gCache = Graphics.FromImage(this.pCanvas.Image);
+
+
+            //this.graphics.CopyFromScreen()
+
+            this.pen = new Pen(this.brush);
+            this.eraser = new Pen(new SolidBrush(Color.White));
+            this.drawType = DrawType.pen;
+            /*Color color = new Color();
+            color.
+            SolidBrush brush = new SolidBrush();*/
+        }
+
+        private void pCanvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            //nastavime stav kde bola stlacena mys
+            if (!isMousePressed)
+            {
+
+                this.startX = e.X;
+                this.startY = e.Y;
+            }
+
+            tempX = e.X;
+            tempY = e.Y;
+            isMousePressed = true;
+
+           // this.graphics.FillRectangle(brush, new Rectangle(e.X, e.Y, 1, 1));
+        }
+
+        private void pCanvas_MouseUp(object sender, MouseEventArgs e)
+        {
+            isMousePressed = false;
+
+           // this.graphics.FillRectangle(brush, new Rectangle(e.X, e.Y, 1, 1));
+        }
+
+        private void pCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+           /* this._x = e.X;
+            this._y = e.Y;
+            return;*/
+            if (!isMousePressed) return;
+
+            int x = e.X;
+            int y = e.Y;
+
+            switch (drawType)
+            {
+                case DrawType.pen:
+                    //this.pCanvas.Image.dr
+                    this.graphics.DrawLine(pen, tempX, tempY, x, y);
+                    this.gCache.DrawLine(pen, tempX, tempY, x, y);
+                    break;
+                case DrawType.circle:
+                    //vymazeme predchadzajuce
+                   
+                    this.graphics.DrawEllipse(this.eraser, this.startX, this.startY, tempX, tempY);
+                    this.graphics.DrawEllipse(pen, this.startX,this.startY, x,y);
+                    break;
+                case DrawType.square:
+                    //vymazeme predchadzjuci
+                    this.graphics.DrawRectangle(this.eraser, this.startX, this.startY, tempX, tempY);
+                    this.graphics.DrawRectangle(this.pen, this.startX, this.startY, x, y);
+                    break;
+            }
+           // this.pCanvas.BackgroundImage = this.graphics.
+
+            //this.gCache.Flush();
+            //this.gCache.Save();
+            //this.pCanvas.Invalidate();
+            
+
+           // this.graphics.FillRectangle(brush, x, y, 1, 1);
+
+            tempX = e.X;
+            tempY = e.Y;
+        }
+
+        private void pCanvas_SizeChanged(object sender, EventArgs e)
+        {
+            if (graphics == null) return;
+
+            //this.graphics = this.pCanvas.CreateGraphics();
+            this.graphics = Graphics.FromImage(this.pCanvas.Image);
+
+
+            if (this.Width > this.frmSize.width || this.Height > this.frmSize.height)
+            {
+                this.graphics = this.gCache;
+                this.graphics = this.pCanvas.CreateGraphics();
+            }
+            
+
+           // graphics.Clip = new Region(r);
+        }
+
+        private void bColorPick_Click(object sender, EventArgs e)
+        {
+            ColorDialog MyDialog = new ColorDialog();
+            // Keeps the user from selecting a custom color.
+            MyDialog.AllowFullOpen = true;
+            // Allows the user to get help. (The default is false.)
+            MyDialog.ShowHelp = true;
+            // Sets the initial color select to the current text color.
+            MyDialog.Color = Color.Black;
+
+            // Update the text box color if the user clicks OK 
+            if (MyDialog.ShowDialog() == DialogResult.OK)
+            {
+                color = MyDialog.Color;
+                brush.Color = color;
+                this.pColor.BackColor = color;
+
+                this.pen = new Pen(new SolidBrush(color));
+            }
+        }
+
+        private void Form1_ResizeBegin(object sender, EventArgs e)
+        {
+            this.gCache = (Graphics)this.graphics;
+        }
+
+        private void Form1_ResizeEnd(object sender, EventArgs e)
+        {
+            this.frmSize.height = this.Height;
+            this.frmSize.width = this.Width;
+           // this.graphics = this.gCache;
+        }
+
+        private void selectObject_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            string[] type = btn.Name.Split('_');
+
+            switch (type[1])
+            {
+                case "pen":
+                    this.drawType = DrawType.pen;
+                    break;
+                case "circle":
+                    this.drawType = DrawType.circle;
+                    break;
+                case "square":
+                    this.drawType = DrawType.square;
+                    break;
+            }
+        }
+
+        private void pCanvas_MouseClick(object sender, MouseEventArgs e)
+        {
+          
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //this.pCanvas.DrawToBitmap(this.gr, this.pCanvas.ClientRectangle);
+
+                //this.pCanvas.Image
+                this.pCanvas.Image.Save(this.saveFileDialog1.FileName, ImageFormat.Jpeg);
+                /*byte[] ar = new byte[ms.Length];
+                ms.Write(ar, 0, ar.Length);
+                ms.Dispose();*/
+               
+                //this.pCanvas.Image.Save(this.saveFileDialog1.FileName, ImageFormat.Jpeg);
+
+            }
+           
+        }
+
+        private void pCanvas_Paint(object sender, PaintEventArgs e)
+        {
+            return;
+            if (!isMousePressed) return;
+
+           
+
+            switch (drawType)
+            {
+                case DrawType.pen:
+                    //this.pCanvas.Image.dr
+                    e.Graphics.DrawLine(pen, tempX, tempY, this._x, this._y);
+                    break;
+                case DrawType.circle:
+                    //vymazeme predchadzajuce
+
+                    e.Graphics.DrawEllipse(this.eraser, this.startX, this.startY, tempX, tempY);
+                    e.Graphics.DrawEllipse(pen, this.startX, this.startY, this._x, this._y);
+                    break;
+                case DrawType.square:
+                    //vymazeme predchadzjuci
+                    e.Graphics.DrawRectangle(this.eraser, this.startX, this.startY, tempX, tempY);
+                    e.Graphics.DrawRectangle(this.pen, this.startX, this.startY, this._x, this._y);
+                    break;
+            }
+            e.Graphics.Flush();
+            e.Graphics.Save();
+            //this.pCanvas.Invalidate();
+
+
+            // this.graphics.FillRectangle(brush, x, y, 1, 1);
+
+            tempX = this._x;
+            tempY = this._y;
+
+           
+        }
+    }
+}
